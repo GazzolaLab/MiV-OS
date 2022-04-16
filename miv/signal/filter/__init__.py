ef75a14b5462b8b4d15673d284a6e4b0be0ea3db
Seung Hyun Kim <skim449@illinois.edu>
2022-04-16 14:29:00 -0500

    move module docstring into doc folder

--- miv/signal/filter/__init__.py
+++ miv/signal/filter/__init__.py
@@ -1,16 +1,3 @@
-__doc__ = """
-Filter
-######
-
-.. autosummary::
-   :nosignatures:
-   :toctree: _toctree/FilterAPI
-
-   FilterProtocol
-   ButterBandpass
-   FilterCollection
-"""
-
 from miv.signal.filter.protocol import *
 from miv.signal.filter.filter_collection import *
 from miv.signal.filter.butter_bandpass_filter import *
