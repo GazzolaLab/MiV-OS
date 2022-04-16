f11919bbb5f82493d5dbde683c60fcce46559349
Seung Hyun Kim <skim449@illinois.edu>
2022-04-15 22:44:46 -0500

    update filters

--- miv/signal/filter/filter_collection.py
+++ miv/signal/filter/filter_collection.py
@@ -9,7 +9,53 @@ import numpy.typing as npt
 
 from collections.abc import Callable, MutableSequence
 
+from miv.typing import SignalType
+
 
 class FilterCollection(MutableSequence, Callable):
-    def __call__(self, array: npt.ArrayLike) -> np.ndarray:
-        raise NotImplementedError  # TODO
+    """Create sequence of multiple filters.
+
+    Each appended filters are expected to abide the FilterProtocol.
+
+        Parameters
+        ----------
+        tag : str
+            Tag for the collection of filter.
+
+        Examples
+        --------
+
+
+    """
+
+    def __init__(self, tag: str = ""):
+        self.filters = []
+        self.tag = tag
+
+    # Callable abstract methods
+    def __call__(self, signal: SignalType) -> SignalType:
+        for filter in self.filters:
+            signal = filter(signal)
+        return signal
+
+    # MutableSequence abstract methods
+    def __len__(self):
+        return len(self.filters)
+
+    def __getitem__(self, idx):
+        return self.filters[idx]
+
+    def __delitem__(self, idx):
+        del self.filters[idx]
+
+    def __setitem__(self, idx, system):
+        self.filters[idx] = system
+
+    def insert(self, idx, system):
+        self.filters.insert(idx, system)
+
+    def __repr__(self):
+        s = f"Collection of filters({self.tag=}, {id(self)=})\n"
+        for filter in self.filters:
+            s += "  " + filter.__repr__() + "\n"
+        return s
--- miv/signal/filter/protocol.py
+++ miv/signal/filter/protocol.py
@@ -1,14 +1,28 @@
-__doc__ = """
-Behavior definition of all filter operator implemented in this directory.
-"""
+__doc__ = ""
 __all__ = ["FilterProtocol"]
 
+import typing
+from typing import Union, Callable, Optional
 from typing import Protocol
 
 import numpy as np
 import numpy.typing as npt
 
+from miv.typing import SignalType
+
 
 class FilterProtocol(Protocol):
-    def __call__(self, array: npt.ArrayLike) -> np.ndarray:
+    """Behavior definition of all filter operator."""
+
+    def __call__(self, array: SignalType, sampling_rate: float, **kwargs) -> SignalType:
+        """User can apply the filter by directly calling.
+        Parameters
+        ----------
+        array : SignalType {"numpy.ndarray", "neo.core.AnalogSignal"}
+        samping_rate : float
+        """
+        ...
+
+    @property
+    def tag(self) -> str:
         ...
