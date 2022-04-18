de4ef3f94f7b0a06484f76d1adcad94d8a510290
Seung Hyun Kim <skim449@illinois.edu>
2022-04-17 20:38:50 -0500

    update: spike detection protocol test

---
+++ tests/spike/test_spike_detection_protocol.py
@@ -0,0 +1,30 @@
+from typing import runtime_checkable
+from typing import Protocol, Any
+
+import pytest
+
+from miv.signal.spike import SpikeDetectionProtocol
+from tests.spike.mock_spike_detection import (
+    mock_spike_detection_list,
+    mock_nonspike_detection_list,
+)
+
+
+@runtime_checkable
+class RuntimeSpikeDetectionProtocol(SpikeDetectionProtocol, Protocol):
+    # This only check the presence of required method, not their type signature.
+    # Check @typing.runtime_checkable documentation for more detail.
+    ...
+
+
+@pytest.mark.parametrize("MockSpikeDetection", mock_spike_detection_list)
+def test_protocol_abide(MockSpikeDetection: SpikeDetectionProtocol):
+    spike_detection: SpikeDetectionProtocol = MockSpikeDetection()
+    assert isinstance(spike_detection, RuntimeSpikeDetectionProtocol)
+
+
+@pytest.mark.parametrize("NonSpikeDetection", mock_nonspike_detection_list)
+def test_non_protocol_filter(NonSpikeDetection: Any):
+    spike_detection: Any = NonSpikeDetection()
+    with pytest.raises(TypeError):
+        issubclass(spike_detection, RuntimeSpikeDetectionProtocol)
