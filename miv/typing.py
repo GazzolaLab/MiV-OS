1b6b73caf76746d435d9a86a1d7c09b266e6220c
Seung Hyun Kim <skim449@illinois.edu>
2022-04-18 02:47:06 -0500

    fix: mypy

--- miv/typing.py
+++ miv/typing.py
@@ -7,7 +7,7 @@ import numpy.typing as npt
 import neo
 
 SignalType = Union[
-    npt.ArrayLike, np.ndarray
+    npt.ArrayLike, np.ndarray, neo.core.AnalogSignal
 ]  # Shape should be [n_channel, signal_length]
 TimestampsType = npt.ArrayLike
 SpikestampsType = Union[npt.ArrayLike, neo.core.SpikeTrain]
