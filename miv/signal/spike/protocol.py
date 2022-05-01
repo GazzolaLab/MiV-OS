8415683090a0ebc27c2b1ca7f9b1ee8ff335b791
Seung Hyun Kim <skim449@illinois.edu>
2022-05-01 14:28:31 -0500

    update protocol

--- miv/signal/spike/protocol.py
+++ miv/signal/spike/protocol.py
@@ -1,7 +1,8 @@
 __all__ = ["SpikeDetectionProtocol"]
 
-from typing import Protocol
+from typing import Protocol, Union, Any, Iterable
 
+import numpy as np
 import neo.core
 
 from miv.typing import SignalType, TimestampsType, SpikestampsType
@@ -14,4 +15,33 @@ class SpikeDetectionProtocol(Protocol):
         ...
 
     def __repr__(self) -> str:
+        ...
+
+
+# TODO: Behavior is clear, but not sure what the name should be
+# class SpikeSortingProtocol(Protocol):
+#    def __call__(
+#        self, signals: Iterable[SignalType]
+#    ) -> np.ndarray:
+#        ...
+#
+#    def __repr__(self) -> str:
+#        ...
+
+
+class SpikeFeatureExtractionProtocol(Protocol):
+    """ex) wavelet transform, PCA, etc."""
+
+    def __repr__(self) -> str:
+        ...
+
+
+class UnsupervisedFeatureClusteringProtocol(Protocol):
+    def __repr__(self) -> str:
+        ...
+
+    def fit(self, X: np.ndarray):
+        ...
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
         ...
