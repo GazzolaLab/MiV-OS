4bd1a6ef6398c10523f57976e4876dba95cebc0b
Seung Hyun Kim <skim449@illinois.edu>
2022-05-01 14:30:31 -0500

    add spike sorting module

---
+++ miv/signal/spike/sorting.py
@@ -0,0 +1,171 @@
+__doc__ = "Spike sorting module"
+__all__ = []
+
+from typing import Any, Union, Optional
+
+from dataclasses import dataclass
+
+import numpy as np
+import scipy
+import scipy.special
+import quantities as pq
+
+import pywt
+
+import matplotlib.pyplot as plt
+
+import neo
+from miv.typing import SignalType, TimestampsType, SpikestampsType
+from miv.signal.spike.protocol import (
+    SpikeFeatureExtractionProtocol,
+    UnsupervisedFeatureClusteringProtocol,
+)
+
+
+class SpikeSorting:
+    """
+    Spike sorting module.
+    User can specify the method for feature extraction (e.g. WaveletDecomposition, PCADecomposition, etc)
+    and the method for clustering (e.g. MeanShift, KMeans, SuperParamagneticClustering, etc).
+
+        spike_sorting = SpikeSorting(
+            feature_extractor=WaveletDecomposition(),
+            clsutering_method=sklearn.cluster.MeanShift()
+        )
+        label, index = spike_sorting(cutouts, return_index=True)
+
+        Parameters
+        ----------
+        feature_extractor : SpikeFeatureExtractionProtocol
+        clustering_method : UnsupervisedFeatureClusteringProtocol
+    """
+
+    def __init__(
+        self,
+        feature_extractor: SpikeFeatureExtractionProtocol,
+        clustering_method: UnsupervisedFeatureClusteringProtocol,
+    ):
+        pass
+
+    def __call__(self):
+        pass
+
+
+## Wavelet Decomposition
+number_of_spikes = 400
+data_length = 100
+cutouts = np.empty([number_of_spikes, data_length])
+spikes_l = cutouts[0]
+coeffs = pywt.wavedec(cutouts, "haar", level=4)
+features = np.concatenate(coeffs, axis=1)
+
+
+def test_ks(x):
+    # Calculates CDF
+
+    # xCDF = []
+    yCDF = []
+    x = x[~np.isnan(x)]
+    n = x.shape[0]
+    x.sort()
+
+    # Get cumulative sums
+    yCDF = (np.arange(n) + 1) / n
+
+    # Remove duplicates; only need final one with total count
+    notdup = np.concatenate([np.diff(x), [1]]) > 0
+    x_expcdf = x[notdup]
+    y_expcdf = np.concatenate([[0], yCDF[notdup]])
+
+    # The theoretical CDF (theocdf) is assumed to ben ormal
+    # with unknown mean and sigma
+    zScore = (x_expcdf - x.mean()) / x.std()
+    # theocdf = normcdf(zScore, 0, 1)
+
+    mu = 0
+    sigma = 1
+    theocdf = 0.5 * scipy.special.erfc(-(zScore - mu) / (np.sqrt(2) * sigma))
+
+    # Compute the Maximum distance: max|S(x) - theocdf(x)|.
+
+    delta1 = (
+        y_expcdf[:-1] - theocdf
+    )  # Vertical difference at jumps approaching from the LEFT.
+    delta2 = (
+        y_expcdf[1:] - theocdf
+    )  # Vertical difference at jumps approaching from the RIGHT.
+    deltacdf = np.abs(np.concatenate([delta1, delta2]))
+
+    KSmax = deltacdf.max()
+    return KSmax
+
+
+ks = []
+for idx, feature in enumerate(np.moveaxis(features, 1, 0)):
+    std_feature = np.std(feature)
+    mean_feature = np.mean(feature)
+    thr_dist = std_feature * 3
+    thr_dist_min = mean_feature - thr_dist
+    thr_dist_max = mean_feature + thr_dist
+    aux = feature[np.logical_and(feature > thr_dist_min, feature < thr_dist_max)]
+
+    if aux.shape[0] > 10:
+        ks.append(test_ks(aux))
+    else:
+        ks.append(0)
+
+max_inputs = 0.75
+min_inputs = 10
+
+# if all:
+# max_inputs = features.shape[1]
+if max_inputs < 1:
+    max_inputs = np.ceil(max_inputs * features.shape[1]).astype(int)
+
+ind = np.argsort(ks)
+A = np.array(ks)[ind]
+A = A[A.shape[0] - max_inputs :]  # Cutoff coeffs
+
+ncoeff = A.shape[0]
+maxA = A.max()
+nd = 10
+d = (A[nd - 1 :] - A[: -nd + 1]) / maxA * ncoeff / nd
+all_above1 = d[np.nonzero(d >= 1)]
+if all_above1.shape[0] >= 2:
+    # temp_bla = smooth(diff(all_above1),3)
+    aux2 = np.diff(all_above1)
+    temp_bla = np.convolve(aux, np.ones(3) / 3)
+    temp_bla = temp_bla[1:-1]
+    temp_bla[0] = aux2[0]
+    temp_bla[-1] = aux2[-1]
+    # ask to be above 1 for 3 consecutive coefficients
+    thr_knee_diff = all_above1[np.nonzero(temp_bla[1:] == 1)[:1]] + nd / 2
+    inputs = max_inputs - thr_knee_diff + 1
+else:
+    inputs = min_inputs
+
+plot_feature_stats = True
+if plot_feature_stats:
+    fig = plt.figure()
+    plt.stairs(np.sort(ks))
+    plt.plot([len(ks) - inputs + 1, len(ks) - inputs + 1], fig.axes[0].get_ylim(), "r")
+    plt.plot(
+        [len(ks) - max_inputs, len(ks) - max_inputs], fig.axes[0].get_ylim(), "--k"
+    )
+    plt.ylabel("ks_stat")
+    plt.xlabel("# features")
+    plt.title(f"number of spikes = {number_of_spikes}, inputs_selected = {inputs}")
+
+
+if inputs > max_inputs:
+    inputs = max_inputs
+elif inputs.size == 0 or inputs < min_inputs:
+    inputs = min_inputs
+
+coeff = ind[-inputs:]
+# CRATES INPUT MATRIX FOR SPC
+input_for_spc = np.zeros((number_of_spikes, inputs))
+
+for i in range(number_of_spikes):
+    for j in range(inputs):
+        input_for_spc[i, j] = features[i, coeff[j]]
