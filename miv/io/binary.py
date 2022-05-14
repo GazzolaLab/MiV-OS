99d3f85964d03c7cc410c878a448659be27fba5f
Seung Hyun Kim <skim449@illinois.edu>
2022-05-14 07:27:37 -0600

    update: binary oe readout module

--- miv/io/binary.py
+++ miv/io/binary.py
@@ -2,16 +2,6 @@ __doc__ = """
 
 -------------------------------------
 
-We expect the data structure to follow the default format exported from OpenEphys system: `format <https://open-ephys.atlassian.net/wiki/spaces/OEW/pages/491632/Data+format>`_.
-
-Original Author
-
-- open-ephys/analysis-tools/Python3/Binary.py (commit: 871e003)
-- original author: malfatti
-  - date: 2019-07-27
-- last modified by: skim449
-  - date: 2022-04-11
-
 Raw Data Loader
 ###############
 
@@ -53,8 +43,68 @@ def apply_channel_mask(signal: np.ndarray, channel_mask: List[int]):
 
         signal[R] = signal[R][:, channel_mask]
 
-    return Data
+    return signal
 
+
+def oebin_read(file_path: str):
+    """
+    Oebin file reader in dictionary form
+
+    Parameters
+    ----------
+    file_path : str
+
+    Returns
+    -------
+    info : Dict[str, any]
+        recording information stored in oebin file.
+    """
+    # TODO: may need fix for multiple continuous data.
+    # TODO: may need to include processor name/id
+    info = literal_eval(open(file_path).read())
+    return info
+
+
+def load_recording(
+    folder: str,
+    channel_mask: Optional[List[int]] = None,
+    unit: Union[str, pq.Quantity] = "uV",
+):
+    """
+    Loads data recorded by Open Ephys in Binary format as numpy memmap.
+    The path should contain
+
+    - continuous/<processor name>/continuous.dat: signal (cannot have multiple file)
+    - continuous/<processor name>/timestamps.dat: timestamps
+    - structure.oebin: number of channels and sampling rate.
+
+    Parameters
+    ----------
+    folder: str
+        folder containing at least the subfolder 'experiment1'.
+    channel_mask: List[int], optional
+        Channel index list to ignore in import (default=None)
+    unit: str or pq.Quantity
+        Unit to return the data, either 'uV' or 'mV', case insensitive. (Default='uV')
+
+    Returns
+    -------
+    signal : SignalType, neo.core.AnalogSignal
+    sampling_rate : float
+
+    """
+
+    file_path: str = glob(os.path.join(folder, "**", "*.dat", recursive=True))
+    assert (
+        len(file_path) == 1
+    ), f"There should be only one 'continuous.dat' file. (There exists {file_path}"
+
+    # load structure information dictionary
+    info_file: str = os.path.join(folder, "structure.oebin")
+    info: Dict[str, Any] = oebin_read(info_file)
+    num_channels: int = info["continuous"][0]["num_channels"]
+    sampling_rate: float = info["continuous"][0]["sample_rate"]
+    # channel_info: Dict[str, Any] = info["continuous"][0]["channels"]
 
     signal, timestamps = load_continuous_data(file_path, num_channels, sampling_rate)
     if channel_mask is not None:
@@ -85,70 +135,76 @@ def _bitsToVolts(Data, ChInfo, Unit):  # TODO: need refactor
     return Data
 
 
-def Load(
-    Folder, Processor=None, Experiment=None, Recording=None, Unit="uV", ChannelMap=[]
+def _load(  # TODO: Need refactor
+    folder, processor=None, experiment=None, recording=None, unit="uV", channel_map=[]
 ):
     """
     Loads data recorded by Open Ephys in Binary format as numpy memmap.
 
-        Load(Folder, Processor=None, Experiment=None, Recording=None, Unit='uV', ChannelMap=[])
+    Here is example usage::
+
+        from miv.io.Binary import load
+
+        folder = '/home/user/<PathToData>/2019-07-27_00-00-00'
+        Data, Rate = load(folder)
+
+        channel_map = [0,15,1,14]
+        recording = 3
+        Data2, Rate2 = load(folder, recording=recording, channel_map=channel_map, unit='Bits')
+
+    Original Author:
+
+    - open-ephys/analysis-tools/Python3/Binary.py (commit: 871e003)
+    - original author: malfatti
+        - date: 2019-07-27
+    - last modified by: skim449
+        - date: 2022-04-11
 
     Parameters
     ----------
-        Folder: str
-            Folder containing at least the subfolder 'experiment1'.
+    folder: str
+        folder containing at least the subfolder 'experiment1'.
 
-        Processor: str or None, optional
-            Processor number to load, according to subsubsubfolders under
-            Folder>experimentX/recordingY/continuous . The number used is the one
-            after the processor name. For example, to load data from the folder
-            'Channel_Map-109_100.0' the value used should be '109'.
-            If not set, load all processors.
+    processor: str or None, optional
+        Processor number to load, according to subsubsubfolders under
+        folder>experimentX/recordingY/continuous . The number used is the one
+        after the processor name. For example, to load data from the folder
+        'Channel_Map-109_100.0' the value used should be '109'.
+        If not set, load all processors.
 
-        Experiment: int or None, optional
-            Experiment number to load, according to subfolders under Folder.
-            If not set, load all experiments.
+    experiment: int or None, optional
+        Experiment number to load, according to subfolders under folder.
+        If not set, load all experiments.
 
-        Recording: int or None, optional
-            Recording number to load, according to subsubfolders under Folder>experimentX .
-            If not set, load all recordings.
+    recording: int or None, optional
+        Recording number to load, according to subsubfolders under folder>experimentX .
+        If not set, load all recordings.
 
-        Unit: str or None, optional
-            Unit to return the data, either 'uV' or 'mV' (case insensitive). In
-            both cases, return data in float32. Defaults to 'uV'.
-            If anything else, return data in int16.
+    unit: str or None, optional
+        Unit to return the data, either 'uV' or 'mV' (case insensitive). In
+        both cases, return data in float32. Defaults to 'uV'.
+        If anything else, return data in int16.
 
-        ChannelMap: list, optional
-            If empty (default), load all channels.
-            If not empty, return only channels in ChannelMap, in the provided order.
-            CHANNELS ARE COUNTED STARTING AT 0.
+    channel_map: list, optional
+        If empty (default), load all channels.
+        If not empty, return only channels in channel_map, in the provided order.
+        CHANNELS ARE COUNTED STARTING AT 0.
 
     Returns
     -------
-        Data: dict
-            Dictionary with data in the structure Data[Processor][Experiment][Recording].
+    Data: dict
+        Dictionary with data in the structure Data[processor][experiment][recording].
+    Rate: dict
+        Dictionary with sampling rates in the structure Rate[processor][experiment].
 
-        Rate: dict
-            Dictionary with sampling rates in the structure Rate[Processor][Experiment].
 
-
-    Example
-    -------
-        import Binary
-
-        Folder = '/home/user/PathToData/2019-07-27_00-00-00'
-        Data, Rate = Binary.Load(Folder)
-
-        ChannelMap = [0,15,1,14]
-        Recording = 3
-        Data2, Rate2 = Binary.Load(Folder, Recording=Recording, ChannelMap=ChannelMap, Unit='Bits')
     """
 
-    Files = sorted(glob(Folder + "/**/*.dat", recursive=True))
-    InfoFiles = sorted(glob(Folder + "/*/*/structure.oebin"))
+    files = sorted(glob(folder + "/**/*.dat", recursive=True))
+    info_file = sorted(glob(folder + "/*/*/structure.oebin"))
 
     Data, Rate = {}, {}
-    for F, File in enumerate(Files):
+    for F, File in enumerate(files):
         File = File.replace("\\", "/")  # Replace windows file delims
         Exp, Rec, _, Proc = File.split("/")[-5:-1]
         Exp = str(int(Exp[10:]) - 1)
@@ -160,16 +216,16 @@ def Load(
         if Proc not in Data.keys():
             Data[Proc], Rate[Proc] = {}, {}
 
-        if Experiment:
-            if int(Exp) != Experiment - 1:
+        if experiment:
+            if int(Exp) != experiment - 1:
                 continue
 
-        if Recording:
-            if int(Rec) != Recording - 1:
+        if recording:
+            if int(Rec) != recording - 1:
                 continue
 
-        if Processor:
-            if Proc != Processor:
+        if processor:
+            if Proc != processor:
                 continue
 
         print("Loading recording", int(Rec) + 1, "...")
@@ -177,7 +233,7 @@ def Load(
             Data[Proc][Exp] = {}
         Data[Proc][Exp][Rec] = np.memmap(File, dtype="int16", mode="c")
 
-        Info = literal_eval(open(InfoFiles[F]).read())
+        Info = literal_eval(open(info_file[F]).read())
         ProcIndex = [
             Info["continuous"].index(_)
             for _ in Info["continuous"]
@@ -198,16 +254,16 @@ def Load(
 
     for Proc in Data.keys():
         for Exp in Data[Proc].keys():
-            if Unit.lower() in ["uv", "mv"]:
+            if unit.lower() in ["uv", "mv"]:
                 ChInfo = Info["continuous"][ProcIndex]["channels"]
-                Data[Proc][Exp] = BitsToVolts(Data[Proc][Exp], ChInfo, Unit)
+                Data[Proc][Exp] = _bitsToVolts(Data[Proc][Exp], ChInfo, unit)
 
-            if ChannelMap:
-                Data[Proc][Exp] = ApplyChannelMap(Data[Proc][Exp], ChannelMap)
+            if channel_map:
+                Data[Proc][Exp] = apply_channel_mask(Data[Proc][Exp], channel_map)
 
     print("Done.")
 
-    return (Data, Rate)
+    return Data, Rate
 
 
 def load_continuous_data(
