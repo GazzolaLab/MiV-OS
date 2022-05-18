ba503ffb4c5390f643e409341ebb2abf68ee59f8
Seung Hyun Kim <skim449@illinois.edu>
2022-05-18 18:11:22 -0500

    update: fix bug for sig2noise ratio

--- miv/statistics/utility.py
+++ miv/statistics/utility.py
@@ -5,17 +5,17 @@ import numpy as np
 from miv.typing import SignalType
 
 
-def signal_to_noise(signal: SignalType, axis: int = 1, ddof: int = 0):
+def signal_to_noise(signal: SignalType, axis: int = 0, ddof: int = 0):
     """signal_to_noise.
 
     Parameters
     ----------
     signal : SignalType
     axis : int
-        Axis of interest. By default, channel axis is 1 (default=1)
+        Axis of interest. By default, signal axis is 0 (default=1)
     ddof : int
     """
     signal_np = np.asanyarray(signal)
     m = signal_np.mean(axis)
     sd = signal_np.std(axis=axis, ddof=ddof)
-    return np.where(sd == 0, 0, m / sd)
+    return np.abs(np.where(sd == 0, 0, m / sd))
