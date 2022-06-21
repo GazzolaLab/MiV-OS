d4a703cd3170d0344f2ebb172db430fd6bbc11d6
Gauravu2 <89496329+Gauravu2@users.noreply.github.com>
2022-06-21 10:01:12 -0500

    Burst analysis for a single channel

--- miv/statistics/__init__.py
+++ miv/statistics/__init__.py
@@ -1,3 +1,5 @@
+from mib.statistics.burst import *
+
 from miv.statistics.pairwise_causality import *
 from miv.statistics.signal_statistics import *
 from miv.statistics.spiketrain_statistics import *
