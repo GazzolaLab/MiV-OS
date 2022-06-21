9b94060958eee6093d001666c12368f7df521e80
Seung Hyun Kim <skim449@illinois.edu>
2022-06-21 01:15:08 -0500

    rename: single causality plot

--- miv/visualization/causality.py
+++ miv/visualization/causality.py
@@ -12,38 +12,40 @@ from miv.typing import SignalType
 
 
 def pairwise_causality_plot(signal: SignalType, start: float, end: float):
+    """
+    Plots pairwise Granger Causality
 
-    # Plots pairwise Granger Causality
+    Parameters
+    ----------
+    signal : SignalType
+        Input signal
+    start : float
+        starting point from signal
+    end : float
+        End point from signal
 
-    # Parameters
-    # ----------
-    # signal : SignalType
-    #    Input signal
-    # start : float
-    #    starting point from signal
-    # end : float
-    #    End point from signal
+    Returns
+    -------
+    figure : matplotlib.pyplot.figure
+        Contains subplots for directional causalities for X -> Y and Y -> X,
+        instantaneous causality between X,Y, and total causality. X and Y
+        represents electrodes
+    axes : matplotlib.axes
+        Figure axes
+    """
 
-    # Returns
-    # -------
-    # figure, axes
-    # Contains subplots for directional causalities for X -> Y and Y -> X,
-    # instantaneous causality between X,Y, and total causality. X and Y represents electrodes
-
-    C_M = pairwise_causality(signal, start, end)
+    # Causality
+    corrcoef_mat = pairwise_causality(signal, start, end)
 
     # Plotting
-
     fig, axes = plt.subplots(2, 2)
     plt.subplots_adjust(
         left=None, bottom=None, right=None, top=None, wspace=0.6, hspace=0.6
     )
-    plot_corrcoef(C[0], axes=axes[0, 0])
-    plot_corrcoef(C[1], axes=axes[0, 1])
-    plot_corrcoef(C[2], axes=axes[1, 0])
-    plot_corrcoef(C[3], axes=axes[1, 1])
-    # axes[:,:].set_xlabel('Electrode')
-    # axes.set_ylabel('Electrode')
+    plot_corrcoef(corrcoef_mat[0], axes=axes[0, 0])
+    plot_corrcoef(corrcoef_mat[1], axes=axes[0, 1])
+    plot_corrcoef(corrcoef_mat[2], axes=axes[1, 0])
+    plot_corrcoef(corrcoef_mat[3], axes=axes[1, 1])
     axes[0, 0].set_xlabel("Electrode")
     axes[0, 1].set_xlabel("Electrode")
     axes[1, 0].set_xlabel("Electrode")
@@ -56,4 +58,5 @@ def pairwise_causality_plot(signal: SignalType, start: float, end: float):
     axes[0, 1].set_title("Directional causality Y => X")
     axes[1, 0].set_title("Instantaneous causality of X,Y")
     axes[1, 1].set_title("Total interdependence of X,Y")
+
     return fig, axes
