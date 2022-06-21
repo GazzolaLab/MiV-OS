7246ca60e90cb042f31de8c92c2e74bbe9a7ee06
jihugo <jiyuge@outlook.com>
2022-06-21 12:07:29 -0500

    change empty list initializers to None

--- miv/io/data.py
+++ miv/io/data.py
@@ -563,9 +563,9 @@ class DataManager(MutableSequence):
         spontaneous_data: Data,
         filter: FilterProtocol,
         detector: SpikeDetectionProtocol,
-        omit_experiments: Iterable[int] = [],
+        omit_experiments: Optional[Iterable[int]] = None,
         spontaneous_offset: float = 0,
-        exp_offsets: List[float] = [],
+        exp_offsets: Optional[List[float]] = None,
         bins_per_second: float = 100,
     ):
         """
