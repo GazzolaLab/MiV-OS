94ce1d98c8d97799fca885d78114a40d798bb807
Gauravu2 <89496329+Gauravu2@users.noreply.github.com>
2022-06-23 01:22:06 -0500

    formatting burst and plotting

--- miv/visualization/__init__.py
+++ miv/visualization/__init__.py
@@ -1,4 +1,4 @@
 from miv.visualization.causality import *
 from miv.visualization.fft_domain import *
-from miv.visualization.waveform import *
 from miv.visualization.plot_burst import *
+from miv.visualization.waveform import *
--- miv/visualization/plot_burst.py
+++ miv/visualization/plot_burst.py
@@ -2,28 +2,29 @@ import os
 
 import matplotlib.pyplot as plt
 import numpy as np
+
 from miv.statistics import burst
 from miv.typing import SpikestampsType
 
 
 def plot_burst(spiketrains: SpikestampsType, min_isi: float, min_len: float):
-    
+
     """
-     Plots burst events across electrodes  to characterize bursting phenomenon on a singl channel
+    Plots burst events across electrodes  to characterize bursting phenomenon on a singl channel
 
-     Parameters
-     ----------
-     spikes : SpikestampsType
-            Single spike-stamps
-     min_isi : float
-        Minimum Interspike Interval (in seconds) to be considered as bursting [standard = 0.1]
-     min_len : float
-        Minimum number of simultaneous spikes to be considered as bursting [standard = 10]
+    Parameters
+    ----------
+    spikes : SpikestampsType
+           Single spike-stamps
+    min_isi : float
+       Minimum Interspike Interval (in seconds) to be considered as bursting [standard = 0.1]
+    min_len : float
+       Minimum number of simultaneous spikes to be considered as bursting [standard = 10]
 
-     Returns
-     -------
-     figure, axes
-        matplot figure with bursts plotted for all electrodes
+    Returns
+    -------
+    figure, axes
+       matplot figure with bursts plotted for all electrodes
     """
     fig, ax = plt.subplots()
     start_time = []
