d97eeb49933fee4290303581c6ad6d511e7f73c1
jihugo <jiyuge@outlook.com>
2022-06-24 15:01:20 -0500

    :construction: creating tests for auto_channel_mask

--- tests/io/mock_data.py
+++ tests/io/mock_data.py
@@ -1,32 +1,39 @@
-from typing import Iterable, Set
+from typing import Iterable, Optional, Set
 
 from contextlib import contextmanager
 
 import numpy as np
 
+# from miv.typing import FilterProtocol, SpikeDetectionProtocol
 from miv.io.data import Data, DataManager
 
-# class MockData(Data):
-#     def __init__(self):
-#         self.masking_channel_set: Set[int] = set()
 
-#     @contextmanager
-#     def load(self):
-#         num_channels = 64
-#         duration: float = 1.5
-#         sampling_rate = 10000
+class MockData(Data):
+    def __init__(self):
+        self.masking_channel_set: Set[int] = set()
 
-#         signal = np.random.rand(int(sampling_rate*duration), num_channels) * 30 - 15
-#         times = np.arange(start=0, stop=duration, step=1/sampling_rate)
+        self.duration: float = 1
+        self.sampling_rate = 10000
+        self.times = np.arange(start=0, stop=self.duration, step=1 / self.sampling_rate)
+        self.signal = []
+        # Channel 0 gives zeroes for signal
+        self.signal.append(np.zeros(self.duration * self.sampling_rate))
+        # signal.append(np.random.rand(int(sampling_rate*duration)) * 30 - 15)
+        self.signal = np.transpose(self.signal)
 
-#         yield signal, times, sampling_rate
-
-#     def set_channel_mask(self, channel_id: Iterable[int]):
-#         self.masking_channel_set.update(channel_id)
+    @contextmanager
+    def load(self):
+        yield self.signal, self.times, self.sampling_rate
 
 
 class MockDataManager(DataManager):
-    def __init__(self, mock_data):
+    def __init__(self, mock_data: Optional[Data] = None):
         self.data_list = []
+        data = mock_data if mock_data else MockData()
         for i in range(2):
-            self.data_list.append(mock_data())
+            self.data_list.append(data)
+
+    def auto_channel_mask_baseline(
+        self, filter, detector, no_spike_threshold: float = 1
+    ):
+        super().auto_channel_mask_baseline(filter, detector, no_spike_threshold)
--- tests/io/test_auto_channel_mask.py
+++ tests/io/test_auto_channel_mask.py
@@ -1,39 +1,72 @@
 import unittest
+from contextlib import contextmanager
 from unittest import mock
-from unittest.mock import call
+from unittest.mock import call, create_autospec
 
+import numpy as np
 import pytest
-from mock_data import MockData, MockDataManager
 
 from miv.io.data import DataManager
 from miv.signal.filter import ButterBandpass
 from miv.signal.spike import ThresholdCutoff
+from tests.io.mock_data import MockData, MockDataManager
 
 
 class TestAutoChannelMask(unittest.TestCase):
     @mock.patch("miv.io.data")
-    def test_get_binned_matrix(self, mock_data):
-
+    def test_spike_detection_was_called_in_baseline(self, mock_object):
         band_filter = ButterBandpass(300, 3000)
-        detector = ThresholdCutoff()
+        detector = mock_object()
+        mock_data = mock_object()
 
-        mock_data.load.return_value = ([], [], 0)
-        mock_data._get_binned_matrix.return_value = {
-            "matrix": [[]],
-            "num_bins": [],
-            "empty_channels": [],
-        }
-        mock_data._auto_channel_mask.side_effect = mock_data._get_binned_matrix(
-            band_filter, detector
-        )
+        @contextmanager
+        def mock_load_funct():
+            times = np.arange(start=0, stop=1, step=1 / 10000.0)
+            signal = np.transpose(np.zeros(10000))
+            yield signal, times, 10000.0
 
-        data_manager = MockDataManager(mock_data)
-        data_manager.auto_channel_mask(mock_data(), band_filter, detector)
+        mock_load = create_autospec(mock_load_funct)
+        mock_data.load.return_value = mock_load()
 
-        calls = [
-            call(band_filter, detector),
-            call(band_filter, detector),
-            call(band_filter, detector),
-        ]
+        manager = MockDataManager(mock_data)
+        manager.auto_channel_mask_baseline(band_filter, detector)
+        detector.assert_has_called()
 
-        mock_data._get_binned_matrix.assert_has_calls(calls)
+    # # @mock.patch("miv.io.data")
+    # def test_auto_channel_mask_baseline(self):
+    #     band_filter = ButterBandpass(300, 3000)
+    #     detector = ThresholdCutoff()
+    #     data_man = MockDataManager()
+
+    #     data_man.auto_channel_mask_baseline(band_filter, detector)
+    #     with data_man[0].load() as (sig, times, samp):
+    #         print(sig)
+    #         print("sig shape", np.shape(sig))
+    #         assert(np.shape(sig)[0] == 0)
+
+    # @mock.patch("miv.io.data")
+    # def test_get_binned_matrix(self, mock_data):
+
+    #     band_filter = ButterBandpass(300, 3000)
+    #     detector = ThresholdCutoff()
+
+    #     mock_data.load.return_value = ([], [], 0)
+    #     mock_data._get_binned_matrix.return_value = {
+    #         "matrix": [[]],
+    #         "num_bins": [],
+    #         "empty_channels": [],
+    #     }
+    #     mock_data._auto_channel_mask.side_effect = mock_data._get_binned_matrix(
+    #         band_filter, detector
+    #     )
+
+    #     data_manager = MockDataManager(mock_data)
+    #     data_manager.auto_channel_mask(mock_data(), band_filter, detector)
+
+    #     calls = [
+    #         call(band_filter, detector),
+    #         call(band_filter, detector),
+    #         call(band_filter, detector),
+    #     ]
+
+    #     mock_data._get_binned_matrix.assert_has_calls(calls)
