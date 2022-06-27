71b192cea1c3a51ef744365c5b37a35b702e44a5
jihugo <jiyuge@outlook.com>
2022-06-27 10:25:24 -0500

    add test_get_binned_matrix_empty_channels

--- tests/io/test_auto_channel_mask.py
+++ tests/io/test_auto_channel_mask.py
@@ -17,8 +17,8 @@ class TestAutoChannelMask(unittest.TestCase):
         band_filter = ButterBandpass(300, 3000)
         detector = ThresholdCutoff()
         data_man = MockDataManager(None)
-
         data_man.auto_channel_mask_baseline(band_filter, detector)
+
         assert data_man[0].masking_channel_set == {0, 1, 2, 3}
         assert data_man[1].masking_channel_set == {0, 1, 2, 3}
 
@@ -26,12 +26,25 @@ class TestAutoChannelMask(unittest.TestCase):
         band_filter = ButterBandpass(300, 3000)
         detector = ThresholdCutoff()
         data_man = MockDataManager(None)
-
         data_man.auto_channel_mask_baseline(band_filter, detector)
+
         data_man[0].clear_channel_mask()
         data_man[1].clear_channel_mask()
         assert len(data_man[0].masking_channel_set) == 0
         assert len(data_man[1].masking_channel_set) == 0
+
+    def test_get_binned_matrix_empty_channels(self):
+        band_filter = ButterBandpass(300, 3000)
+        detector = ThresholdCutoff()
+        data_man = MockDataManager(None)
+        data_man.auto_channel_mask_baseline(band_filter, detector)
+
+        assert data_man[0]._get_binned_matrix(band_filter, detector)[
+            "empty_channels"
+        ] == [0, 1, 2, 3]
+        assert data_man[1]._get_binned_matrix(band_filter, detector)[
+            "empty_channels"
+        ] == [0, 1, 2, 3]
 
     # def test_auto_channel_mask(self):
     #     band_filter = ButterBandpass(300, 3000)
