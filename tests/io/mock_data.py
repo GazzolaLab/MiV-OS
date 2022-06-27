98000a7a0b02c925facb47510ca9761095ec193d
jihugo <jiyuge@outlook.com>
2022-06-27 10:03:58 -0500

    add test_clear_channel_masks

--- tests/io/mock_data.py
+++ tests/io/mock_data.py
@@ -16,10 +16,20 @@ class MockData(Data):
         self.sampling_rate = 10000
         self.times = np.arange(start=0, stop=self.duration, step=1 / self.sampling_rate)
         self.signal = []
-        # Channels 0 and 1 give zeroes for signal
-        self.signal.append(np.zeros(self.duration * self.sampling_rate))
-        self.signal.append(np.zeros(self.duration * self.sampling_rate))
-        # signal.append(np.random.rand(int(sampling_rate*duration)) * 30 - 15)
+        # Channels 0, 1, 2, and 3 give zeroes for signal
+        zero_signal = np.zeros(self.duration * self.sampling_rate)
+        self.signal.append(zero_signal)
+        self.signal.append(zero_signal)
+        self.signal.append(zero_signal)
+        self.signal.append(zero_signal)
+        # Channel 4 has 10 spikes
+        ten_spikes_signal = zero_signal.copy()
+        for i in range(999, 10000, 1000):
+            ten_spikes_signal[i] = 1000
+            ten_spikes_signal[i - 1] = 1000
+            ten_spikes_signal[i - 2] = 1000
+        self.signal.append(ten_spikes_signal)
+
         self.signal = np.transpose(self.signal)
 
     @contextmanager
--- tests/io/test_auto_channel_mask.py
+++ tests/io/test_auto_channel_mask.py
@@ -13,58 +13,30 @@ from tests.io.mock_data import MockData, MockDataManager
 
 
 class TestAutoChannelMask(unittest.TestCase):
-    # @mock.patch("miv.io.data")
-    # def test_spike_detection_was_called_in_baseline(self, mock_object):
-    #     band_filter = ButterBandpass(300, 3000)
-    #     detector = mock_object()
-    #     mock_data = mock_object()
-
-    #     @contextmanager
-    #     def mock_load_funct():
-    #         times = np.arange(start=0, stop=1, step=1 / 10000.0)
-    #         signal = np.transpose(np.zeros(10000))
-    #         yield signal, times, 10000.0
-
-    #     mock_load = create_autospec(mock_load_funct)
-    #     mock_data.load.return_value = mock_load()
-
-    #     manager = MockDataManager(mock_data)
-    #     manager.auto_channel_mask_baseline(band_filter, detector)
-    #     detector.assert_has_called()
-
-    # @mock.patch("miv.io.data")
     def test_auto_channel_mask_baseline(self):
         band_filter = ButterBandpass(300, 3000)
         detector = ThresholdCutoff()
         data_man = MockDataManager(None)
 
         data_man.auto_channel_mask_baseline(band_filter, detector)
-        assert data_man[0].masking_channel_set == {0, 1}
-        assert data_man[1].masking_channel_set == {0, 1}
+        assert data_man[0].masking_channel_set == {0, 1, 2, 3}
+        assert data_man[1].masking_channel_set == {0, 1, 2, 3}
 
-    # @mock.patch("miv.io.data")
-    # def test_get_binned_matrix(self, mock_data):
+    def test_clear_channel_masks(self):
+        band_filter = ButterBandpass(300, 3000)
+        detector = ThresholdCutoff()
+        data_man = MockDataManager(None)
 
+        data_man.auto_channel_mask_baseline(band_filter, detector)
+        data_man[0].clear_channel_mask()
+        data_man[1].clear_channel_mask()
+        assert len(data_man[0].masking_channel_set) == 0
+        assert len(data_man[1].masking_channel_set) == 0
+
+    # def test_auto_channel_mask(self):
     #     band_filter = ButterBandpass(300, 3000)
     #     detector = ThresholdCutoff()
+    #     data_man = MockDataManager(None)
 
-    #     mock_data.load.return_value = ([], [], 0)
-    #     mock_data._get_binned_matrix.return_value = {
-    #         "matrix": [[]],
-    #         "num_bins": [],
-    #         "empty_channels": [],
-    #     }
-    #     mock_data._auto_channel_mask.side_effect = mock_data._get_binned_matrix(
-    #         band_filter, detector
-    #     )
-
-    #     data_manager = MockDataManager(mock_data)
-    #     data_manager.auto_channel_mask(mock_data(), band_filter, detector)
-
-    #     calls = [
-    #         call(band_filter, detector),
-    #         call(band_filter, detector),
-    #         call(band_filter, detector),
-    #     ]
-
-    #     mock_data._get_binned_matrix.assert_has_calls(calls)
+    #     data_man.auto_channel_mask(data_man[0], band_filter, detector)
+    #     assert data_man[0].masking_channel_set == {0, 1, 2, 3}
