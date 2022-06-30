8ba8d3b2edf2082de3731850aa3edb0d5f859aa3
Gauravu2 <89496329+Gauravu2@users.noreply.github.com>
2022-06-30 00:14:18 -0500

    updated init file

--- miv/visualization/__init__.py
+++ miv/visualization/__init__.py
@@ -1,4 +1,5 @@
 from miv.visualization.causality import *
 from miv.visualization.event import *
 from miv.visualization.fft_domain import *
+from miv.visualization.spike_triggered_avg import *
 from miv.visualization.waveform import *
