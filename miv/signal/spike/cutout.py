ce2d95c71408feb32e747b103979c735d34317fc
jihugo <jiyuge@outlook.com>
2022-06-30 23:29:25 -0500

    :construction: add ChannelSpikeCutout class

--- miv/signal/spike/cutout.py
+++ miv/signal/spike/cutout.py
@@ -1,6 +1,6 @@
-__all__ = ["SpikeCutout"]
+__all__ = ["SpikeCutout", "ChannelSpikeCutout"]
 
-from typing import Tuple, Union
+from typing import List, Optional, Tuple, Union
 
 from dataclasses import dataclass
 
@@ -15,18 +15,31 @@ class SpikeCutout:
     ----------
     cutout : np.ndarray
     sampling_rate : float
-    category : int
-        (default = 0)
+    pca_comp_index : int
     """
-
-    CATEGORY_NAMES: tuple[str] = ("uncategorized", "neuronal", "false", "mixed")
 
     def __init__(
         self,
         cutout: Union[np.ndarray, Tuple[np.ndarray, np.ndarray]],
         sampling_rate: float,
-        category: int = 0,
+        pca_comp_index: int,
     ) -> None:
         self.cutout: np.ndarray = cutout
         self.sampling_rate: float = sampling_rate
-        self.category: int = category
+        self.pca_comp_index: int = pca_comp_index
+
+
+class ChannelSpikeCutout:
+    CATEGORY_NAMES: tuple[str] = ("uncategorized", "neuronal", "false", "mixed")
+
+    def __init__(
+        self,
+        cutouts: List[SpikeCutout],
+        num_components: int,
+        categorized: bool = False,
+        category_list: Optional[np.ndarray] = None,
+    ):
+        self.cutouts: List[SpikeCutout] = cutouts
+        self.num_components: int = num_components
+        self.categorized: bool = categorized
+        self.category_list = category_list if categorized else np.zeros(num_components)
