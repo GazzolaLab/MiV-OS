2cae6ce92bc7d9fd7bad8d3bd6707c9cd9a1ee3a
jihugo <jiyuge@outlook.com>
2022-06-30 23:45:55 -0500

    :construction: edit _get_cutouts()

--- miv/signal/events/abnormality_detection.py
+++ miv/signal/events/abnormality_detection.py
@@ -6,7 +6,12 @@ from tqdm import tqdm
 
 from miv.io import Data, DataManager
 from miv.signal.filter import ButterBandpass, FilterProtocol
-from miv.signal.spike import SpikeCutout, SpikeDetectionProtocol, ThresholdCutoff
+from miv.signal.spike import (
+    ChannelSpikeCutout,
+    PCADecomposition,
+    SpikeCutout,
+    SpikeDetectionProtocol,
+)
 from miv.typing import SpikestampsType
 from miv.visualization import extract_waveforms
 
@@ -26,23 +31,36 @@ class AbnormalityDetector:
         self.spike_detector = spike_detector
         self.num_components: int = pca_num_components
         self.trained: bool = False
+        self.categorized: bool = False
 
         # 1. Generate PCA cutouts for spontaneous recording
-        # spontanous_cutouts = self.get_cutouts(spontaneous_data)
+        self.spontanous_cutouts = self._get_cutouts(spontaneous_data)
 
-    def get_cutouts(self, data: Data) -> List[SpikeCutout]:
+    def _get_cutouts(self, data: Data) -> List[ChannelSpikeCutout]:
+        pca = PCADecomposition()
         with data.load() as (sig, times, samp):
             spontaneous_sig = self.signal_filter(sig, samp)
             spontaneous_spikes = self.spike_detector(spontaneous_sig, times, samp)
 
             skipped_channels = []  # Channels with not enough spikes for cutouts
-            cutouts = []  # List of SpikeCutout objects
+            exp_cutouts = []  # List of SpikeCutout objects
             for chan_index in tqdm(range(spontaneous_sig.shape[1])):
                 try:
-                    channel_cutouts = extract_waveforms(
+                    channel_cutouts_list: List[SpikeCutout] = []
+                    raw_cutouts = extract_waveforms(
                         spontaneous_sig, spontaneous_spikes, chan_index, samp
                     )
-                    cutouts.append(SpikeCutout(channel_cutouts, samp))
+                    labels, transformed = pca.project(self.num_components, raw_cutouts)
+
+                    for cutout_index, raw_cutout in enumerate(raw_cutouts):
+                        channel_cutouts_list.append(
+                            SpikeCutout(raw_cutout, samp, labels[cutout_index])
+                        )
+                    exp_cutouts.append(
+                        ChannelSpikeCutout(channel_cutouts_list, self.num_components)
+                    )
                 except ValueError:
                     skipped_channels.append(chan_index)
-        return cutouts
+        return exp_cutouts
+
+    # def categorize_spontaneous(self, category_list):
