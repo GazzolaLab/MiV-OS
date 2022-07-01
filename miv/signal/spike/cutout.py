9ab866cf8a0b2f88654462fbcba034c6b53bc7ae
jihugo <jiyuge@outlook.com>
2022-06-30 20:20:01 -0500

    fix Spikecutout initializer

--- miv/signal/spike/cutout.py
+++ miv/signal/spike/cutout.py
@@ -22,7 +22,11 @@ class SpikeCutout:
         (default = 0)
     """
 
-    cutout: np.ndarray = cutout
-    sampling_rate: float = sampling_rate
-    category: int = 0
     CATEGORY_NAMES: tuple[str] = ("uncategorized", "neuronal", "false", "mixed")
+
+    def __init__(
+        self, cutout: np.ndarray, sampling_rate: float, category: int = 0
+    ) -> None:
+        self.cutout: np.ndarray = cutout
+        self.sampling_rate: float = sampling_rate
+        self.category: int = category
