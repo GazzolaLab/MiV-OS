7862a368c16d4ad5482afef85101a95b9ad7154e
jihugo <jiyuge@outlook.com>
2022-07-07 23:10:47 -0500

    :construction: add test_categorize() and test_labeled_cutouts()

--- tests/spike/test_cutout.py
+++ tests/spike/test_cutout.py
@@ -51,3 +51,41 @@ def test_get_cutouts_by_components():
 
     cutouts_by_components = chan_spike_cutout.get_cutouts_by_component()
     assert np.shape(cutouts_by_components) == (3, 2)
+
+
+def test_categorize():
+    cutouts = []
+    cutouts.append(MockSpikeCutout(0, 0, 0))
+    cutouts.append(MockSpikeCutout(1, 1, 0.1))
+    cutouts.append(MockSpikeCutout(2, 2, 0.2))
+    cutouts.append(MockSpikeCutout(0, 0, 0.3))
+    cutouts.append(MockSpikeCutout(1, 1, 0.4))
+    cutouts.append(MockSpikeCutout(2, 2, 0.5))
+    chan_spike_cutout = ChannelSpikeCutout(cutouts, 3, 0)
+
+    cat_list = [2, 1, 0]
+    chan_spike_cutout.categorize(cat_list)
+    for spike_index, spike_cutout in enumerate(chan_spike_cutout.cutouts):
+        assert spike_cutout.categorized
+        assert (
+            chan_spike_cutout.categorization_list[spike_cutout.pca_comp_index]
+            == cat_list[spike_cutout.pca_comp_index]
+        )
+
+
+def test_labeled_cutouts():
+    cutouts = []
+    cutouts.append(MockSpikeCutout(0, 0, 0, 100))
+    cutouts.append(MockSpikeCutout(1, 1, 0.1, 100))
+    cutouts.append(MockSpikeCutout(2, 2, 0.2, 100))
+    cutouts.append(MockSpikeCutout(0, 0, 0.3, 100))
+    cutouts.append(MockSpikeCutout(1, 1, 0.4, 100))
+    cutouts.append(MockSpikeCutout(2, 2, 0.5, 100))
+    chan_spike_cutout = ChannelSpikeCutout(cutouts, 3, 0)
+
+    cat_list = [-1, 1, 0]
+    chan_spike_cutout.categorize(cat_list)
+    labeled_cutouts = chan_spike_cutout.get_labeled_cutouts()
+    assert np.shape(labeled_cutouts["labels"]) == (4,)
+    assert np.shape(labeled_cutouts["labeled_cutouts"]) == (4, 100)
+    assert labeled_cutouts["size"] == 4
