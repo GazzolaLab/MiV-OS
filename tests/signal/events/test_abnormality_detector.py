2f8d7c3889200623f61320dd8109754bfb430b77
jihugo <jiyuge@outlook.com>
2022-07-09 21:44:26 -0500

    :construction: add test_train_model(), found error with train_model()

--- tests/signal/events/test_abnormality_detector.py
+++ tests/signal/events/test_abnormality_detector.py
@@ -3,6 +3,7 @@ from typing import List
 from unittest import mock
 
 import numpy as np
+import tensorflow as tf
 
 from miv.signal.events.abnormality_detection import AbnormalityDetector
 from miv.signal.spike.cutout import ChannelSpikeCutout, SpikeCutout
@@ -38,7 +39,7 @@ def test_categorize_spontaneous():
         cutouts.append(MockSpikeCutout(1, 1, 0.4))
         cutouts.append(MockSpikeCutout(2, 2, 0.5))
         chan_spike_cutouts.append(ChannelSpikeCutout(cutouts, 3, 0))
-    abn_detector = MockAbnormalDetector(chan_spike_cutouts, 3, 6)
+    abn_detector = MockAbnormalDetector(chan_spike_cutouts, 3, 2)
 
     cat_list = [[2, 1, 0], [2, 1, 0]]
     abn_detector.categorize_spontaneous(cat_list)
@@ -47,3 +48,34 @@ def test_categorize_spontaneous():
     for chan_index, chan_spike_cutout in enumerate(abn_detector.spontaneous_cutouts):
         for cutout_index, elem in enumerate(chan_spike_cutout.categorization_list):
             assert elem == cat_list[chan_index][cutout_index]
+
+
+def test_train_model():
+    chan_spike_cutouts = []
+    for i in range(2):
+        cutouts = []
+        cutouts.append(MockSpikeCutout(0, 0, 0))
+        cutouts.append(MockSpikeCutout(1, 1, 0.1))
+        cutouts.append(MockSpikeCutout(2, 2, 0.2))
+        cutouts.append(MockSpikeCutout(0, 0, 0.3))
+        cutouts.append(MockSpikeCutout(1, 1, 0.4))
+        cutouts.append(MockSpikeCutout(2, 2, 0.5))
+        chan_spike_cutouts.append(ChannelSpikeCutout(cutouts, 3, 0))
+    abn_detector = MockAbnormalDetector(chan_spike_cutouts, 3, 2)
+
+    abn_detector.categorize_spontaneous([[-1, 1, 0], [-1, 1, 0]])
+    abn_detector.train_model([16], 3)
+
+    test_cutout0 = np.array(MockSpikeCutout(0, 0, 0.6).cutout)
+    test_cutout1 = np.array(MockSpikeCutout(1, 1, 0.7).cutout)
+    test_cutout2 = np.array(MockSpikeCutout(2, 2, 0.8).cutout)
+
+    prob_model = tf.keras.Sequential([abn_detector.model, tf.keras.layers.Softmax()])
+
+    prediction0 = prob_model.predict(test_cutout0, verbose=0)
+    prediction1 = prob_model.predict(test_cutout1, verbose=0)
+    prediction2 = prob_model.predict(test_cutout2, verbose=0)
+
+    assert np.argmax(prediction0) == -1
+    assert np.argmax(prediction1) == 1
+    assert np.argmax(prediction2) == 0
