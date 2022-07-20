e74497b34069b45081f0aa4cc521fa5447eead8b
Seung Hyun Kim <skim449@illinois.edu>
2022-07-20 02:30:19 -0500

    change theme

--- docs/conf.py
+++ docs/conf.py
@@ -97,7 +97,7 @@ master_doc = "index"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = "sphinx_book_theme"
+html_theme = "pydata_sphinx_theme"
 html_theme_options = {
     "repository_url": "https://github.com/GazzolaLab/MiV-OS",
     "use_repository_button": True,
