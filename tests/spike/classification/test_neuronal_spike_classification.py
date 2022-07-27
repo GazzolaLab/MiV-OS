1441c73da6c9169f8740247f5ada7ba7c1e2378b
jihugo <jiyuge@outlook.com>
2022-07-27 16:42:16 -0500

    :construction: add test_predict_categories()

--- tests/spike/classification/test_neuronal_spike_classification.py
+++ tests/spike/classification/test_neuronal_spike_classification.py
@@ -36,13 +36,29 @@ def test_default_model():
     classifier.train_model(x=mock_spikes.spikes, y=mock_spikes.labels, epochs=5)
 
 
-# def test_get_confusion_matrix():
-#     mock_spikes = MockSpikes(size=9)
-#     classifier = NeuronalSpikeClassifier()
-#     classifier.create_default_tf_keras_model(
-#         mock_spikes.spikes, mock_spikes.labels
-#     )
-#     classifier.default_compile_model()
-#     classifier.train_model(
-#         mock_spikes.spikes, mock_spikes.labels
-#     )
+class DistinctiveMockSpikes:
+    def __init__(self, size: int = 40):
+        spike0 = np.ones(size)
+        spike1 = np.linspace(start=-10, stop=10, num=size)
+        self.spikes = np.array([spike0, spike1, spike0, spike1])
+        self.labels = np.array([0, 1, 0, 1])
+
+
+def test_predict_categories():
+    mock_spikes = DistinctiveMockSpikes(size=30)
+    classifier = NeuronalSpikeClassifier()
+    classifier.create_default_tf_keras_model(mock_spikes.spikes, mock_spikes.labels)
+    classifier.default_compile_model()
+    classifier.train_model(
+        x=mock_spikes.spikes,
+        y=mock_spikes.labels,
+        epochs=500,  # Just 4 samples per epoch so this should be fine
+    )
+
+    test_spikes = mock_spikes.spikes
+    test_labels = mock_spikes.labels
+
+    predictions = classifier.predict_categories(test_spikes)
+
+    for spike_index, spike_prediction in enumerate(predictions):
+        assert test_labels[spike_index] == np.argmax(spike_prediction)
