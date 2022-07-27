60104578718aeeab5387891453e9916fa9127934
Gauravu2 <89496329+Gauravu2@users.noreply.github.com>
2022-07-27 00:52:31 -0500

    Update __init__.py

--- miv/visualization/__init__.py
+++ miv/visualization/__init__.py
@@ -3,6 +3,7 @@ from miv.visualization.connectivity_plot import *
 from miv.visualization.event import *
 from miv.visualization.fft_domain import *
 from miv.visualization.spike_triggered_avg import *
+from miv.visualization.raw_signal import *
 from miv.visualization.waveform import *
 
 # TODO:
