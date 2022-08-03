db5dbdd5061f0e1750306d334b3b81d0574a0f6d
Bhosale <bhosale2@illinois.edu>
2022-08-03 01:04:48 -0500

    remove fft viz code repeat and add tests for rest of the function

--- miv/visualization/fft_domain.py
+++ miv/visualization/fft_domain.py
@@ -1,7 +1,5 @@
 __all__ = ["plot_frequency_domain", "plot_spectral"]
 
-import os
-
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import fftpack
@@ -10,7 +8,7 @@ from scipy.signal import coherence, csd, welch
 from miv.typing import SignalType
 
 
-def plot_frequency_domain(signal: SignalType, sampling_rate: float) -> plt.Figure:
+def plot_frequency_domain(signal: SignalType) -> plt.Figure:
     """
     Plot DFT frequency domain
 
@@ -18,8 +16,6 @@ def plot_frequency_domain(signal: SignalType, sampling_rate: float) -> plt.Figur
     ----------
     signal : SignalType
         Input signal
-    sampling_rate : float
-        Sampling frequency
 
     Returns
     -------
@@ -29,20 +25,9 @@ def plot_frequency_domain(signal: SignalType, sampling_rate: float) -> plt.Figur
     # FFT
     fig = plt.figure()
     sig_fft = fftpack.fft(signal)
-    # sample_freq = fftpack.fftfreq(signal.size, d=1 / sampling_rate)
     plt.plot(np.abs(sig_fft) ** 2)
     plt.xlabel("Frequency [Hz]")
     plt.ylabel("DFT frequency")
-
-    # Welch (https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.welch.html)
-    f, Pxx_den = welch(signal, sampling_rate, nperseg=1024)
-    f_med, Pxx_den_med = welch(signal, sampling_rate, nperseg=1024, average="median")
-    plt.figure()
-    plt.semilogy(f, Pxx_den, label="mean")
-    plt.semilogy(f_med, Pxx_den_med, label="median")
-    plt.xlabel("frequency [Hz]")
-    plt.ylabel("PSD [uV**2/Hz]")
-    plt.legend()
     return fig
 
 
