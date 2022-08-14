c79683af07b5b1e017a3b7587a3948c0925519db
Gauravu2 <89496329+Gauravu2@users.noreply.github.com>
2022-08-13 23:09:56 -0500

    connectivity_tests

--- tests/visualization/test_plot_connectivity.py
+++ tests/visualization/test_plot_connectivity.py
@@ -12,8 +12,10 @@ def test_plot_connectivity_output():
     # Initialize the spiketrain as below
     mea_map = np.array([[1, 2], [3, 4], [5, 6]])
     mea_map1 = np.array([[0, 0], [0, 0], [0, 0]])
+    mea_map_t = np.array([[1, 2]])
     connectivity_matrix1 = np.array([[2, 3], [10, 11]])
     connectivity_matrix2 = np.array([[2, 3], [5, 8], [9, 10]])
+    connectivity_matrix_t = np.array([[0, 0.1], [0.1, 0]])
 
     with pytest.raises(AssertionError):
         plot_connectivity(mea_map, connectivity_matrix1, False)
@@ -24,6 +26,10 @@ def test_plot_connectivity_output():
     with pytest.raises(AssertionError):
         plot_connectivity(mea_map1, connectivity_matrix2, True)
         # The function above should throw an error since MEA map contains no identification
+    output = plot_connectivity(mea_map_t, connectivity_matrix_t, False)
+    assert output[0].engine == "neato"
+    output = plot_connectivity(mea_map_t, connectivity_matrix_t, True)
+    assert output[0].engine == "neato"
 
 
 def test_plot_connectivity_interactive_output():
@@ -31,8 +37,10 @@ def test_plot_connectivity_interactive_output():
     # Initialize the spiketrain as below
     mea_map = np.array([[1, 2], [3, 4], [5, 6]])
     mea_map1 = np.array([[0, 0], [0, 0], [0, 0]])
+    mea_map_t = np.array([[1, 2]])
     connectivity_matrix1 = np.array([[2, 3], [10, 11]])
     connectivity_matrix2 = np.array([[2, 3], [5, 8], [9, 10]])
+    connectivity_matrix_t = np.array([[0, 0.1], [0.1, 0]])
 
     with pytest.raises(AssertionError):
         plot_connectivity_interactive(mea_map, connectivity_matrix1, False)
@@ -43,3 +51,8 @@ def test_plot_connectivity_interactive_output():
     with pytest.raises(AssertionError):
         plot_connectivity_interactive(mea_map1, connectivity_matrix2, True)
         # The function above should throw an error since MEA map contains no identification
+
+    output = plot_connectivity_interactive(mea_map_t, connectivity_matrix_t, False)
+    assert output[0].get_edges()[0]["width"] == "1"
+    output = plot_connectivity_interactive(mea_map_t, connectivity_matrix_t, True)
+    assert output[0].get_edges()[0]["width"] == "1"
