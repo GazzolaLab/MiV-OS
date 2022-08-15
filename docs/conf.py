d41ac21b22970921f40b7225123d058ef2c3c2d6
Seung Hyun Kim <skim449@illinois.edu>
2022-08-15 05:51:37 -0500

    fix url prefix

--- docs/conf.py
+++ docs/conf.py
@@ -98,6 +98,7 @@ master_doc = "index"
 html_theme = "pydata_sphinx_theme"
 html_logo = "https://raw.githubusercontent.com/skim0119/MiV-OS/assets_logo/docs/_static/assets/logo1.svg"
 html_favicon = "https://raw.githubusercontent.com/skim0119/MiV-OS/assets_logo/docs/_static/assets/logo1.svg"
+html_sourcelink_suffix = ""
 html_theme_options = {
     "logo": {
         # "link": "https://mindinvitro.illinois.edu/",
