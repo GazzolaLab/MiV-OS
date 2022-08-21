420582ab919b5a75ba9b5207b0c64a3d9236f64d
Seung Hyun Kim <skim449@illinois.edu>
2022-08-20 23:17:17 -0500

    update logo url

--- docs/conf.py
+++ docs/conf.py
@@ -136,7 +136,7 @@ html_context = {"default_mode": "dark"}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static", "_static/assets"]
+html_static_path = ["_static"]
 # html_css_files = ["css/*", "css/logo.css"]
 
 # -- Options for numpydoc ---------------------------------------------------
