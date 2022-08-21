8266eef8e6558b2caa81266887fa2897dbc4a5c0
Seung Hyun Kim <skim449@illinois.edu>
2022-08-21 16:30:31 -0500

    add script as executable command

--- scripts/convert_open_ephys_to_miv.py
+++ scripts/convert_open_ephys_to_miv.py
@@ -1,12 +1,15 @@
 # A script to convert Open Ephys data to MiV HDF5 format.
 
-import click
+import logging
 import os
 import sys
-import logging
+
+import click
 import numpy as np
+
 from miv.io import Data, DataManager
 from miv.io import file as miv_file
+
 
 def config_logging(verbose):
     if verbose:
@@ -14,7 +17,9 @@ def config_logging(verbose):
     else:
         logging.basicConfig(level=logging.WARN)
 
+
 script_name = os.path.basename(__file__)
+
 
 def seq_find(f, seq):
     """
@@ -34,15 +39,16 @@ def seq_find(f, seq):
             i = i + 1
     return None
 
+
 @click.command()
-@click.option("--folder-path", '-p', required=True, type=click.Path())
+@click.option("--folder-path", "-p", required=True, type=click.Path())
 @click.option("--verbose", "-v", type=bool, default=False, is_flag=True)
 def main(folder_path: str, verbose: bool):
     """Convert Open Ephys data to MiV HDF5 format"""
 
     config_logging(verbose)
-    logger = logging.getLogger('miv_file')
-    
+    logger = logging.getLogger("miv_file")
+
     # Load dataset from OpenEphys recording
     data_manager = DataManager(folder_path)
     miv_data = miv_file.initialize()
@@ -56,16 +62,29 @@ def main(folder_path: str, verbose: bool):
         data_path = recording.data_path
         with recording.load() as (signal, timestamps, sampling_rate):
             group_id = miv_file.create_group(miv_data, data_path, counter="nrecording")
-            miv_file.create_dataset(miv_data, ["signal", "timestamps", "sampling_rate"], group=data_path, dtype=float)
+            miv_file.create_dataset(
+                miv_data,
+                ["signal", "timestamps", "sampling_rate"],
+                group=data_path,
+                dtype=float,
+            )
             cont[f"{group_id}/signal"] = signal
             cont[f"{group_id}/timestamps"] = timestamps
             cont[f"{group_id}/sampling_rate"] = [sampling_rate]
             miv_file.pack(miv_data, cont, logger=logger)
-            
-    f = miv_file.write(f"{folder_path}/MiV_data.h5", miv_data, comp_type="gzip", comp_opts=9, logger=logger)
 
-        
-if __name__ == '__main__':
-    main(args=sys.argv[(seq_find(lambda x: os.path.basename(x) == script_name, sys.argv)+1):])
+    miv_file.write(
+        f"{folder_path}/MiV_data.h5",
+        miv_data,
+        comp_type="gzip",
+        comp_opts=9,
+        logger=logger,
+    )
 
-        
+
+if __name__ == "__main__":
+    main(
+        args=sys.argv[
+            (seq_find(lambda x: os.path.basename(x) == script_name, sys.argv) + 1) :
+        ]
+    )
