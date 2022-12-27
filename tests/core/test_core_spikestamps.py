51e653402a38cd8478e80743c7112c241c6b4620
Seung Hyun Kim <skim449@illinois.edu>
2022-12-26 21:04:15 -0600

    test: add test for core.spikestamps

---
+++ tests/core/test_core_spikestamps.py
@@ -0,0 +1,72 @@
+from collections import UserList
+
+import pytest
+
+from miv.core import Spikestamps
+
+
+def test_spikestamps_init():
+    # Test that the data attribute is set correctly
+    s = Spikestamps([[1, 2, 3], [4, 5, 6]])
+    assert s.data == [[1, 2, 3], [4, 5, 6]]
+    assert isinstance(s, UserList)
+
+    # Test that the data attribute is set to an empty list if no iterable is provided
+    s = Spikestamps([])
+    assert s.data == []
+
+
+def test_spikestamps_setitem():
+    s = Spikestamps([[1, 2, 3], [4, 5, 6]])
+
+    # Test that we can set the value of an existing element
+    s[0] = [7, 8, 9]
+    assert s[0] == [7, 8, 9]
+
+    # Test that an IndexError is raised if the index is out of range
+    with pytest.raises(IndexError):
+        s[2] = [10, 11, 12]
+
+
+def test_spikestamps_insert():
+    s = Spikestamps([[1, 2, 3], [4, 5, 6]])
+
+    # Test that we can insert an element at the beginning of the list
+    s.insert(0, [7, 8, 9])
+    assert s == [[7, 8, 9], [1, 2, 3], [4, 5, 6]]
+
+    # Test that we can insert an element at the end of the list
+    s.insert(3, [10, 11, 12])
+    assert s == [[7, 8, 9], [1, 2, 3], [4, 5, 6], [10, 11, 12]]
+
+    # Test that an IndexError is raised if the index is out of range
+    with pytest.raises(IndexError):
+        s.insert(10, [13, 14, 15])
+
+
+def test_spikestamps_append():
+    s = Spikestamps([[1, 2, 3], [4, 5, 6]])
+
+    # Test that we can append an element to the end of the list
+    s.append([7, 8, 9])
+    assert s == [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+
+def test_spikestamps_extend():
+    s1 = Spikestamps([[1, 2, 3], [4, 5, 6]])
+    s2 = [[7, 8, 9], [10, 11, 12]]
+
+    # Test that we can extend the current object with another Spikestamps object
+    s1.extend(s2)
+    assert s1 == [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+
+    # Test that we can extend the current object with an iterable of arrays
+    s1.extend([[13, 14, 15], [16, 17, 18]])
+    assert s1 == [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [10, 11, 12],
+        [13, 14, 15],
+        [16, 17, 18],
+    ]
