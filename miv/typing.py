40b33f84a9b4962f4c2c825e049d8687723eeb3c
Seung Hyun Kim <skim449@illinois.edu>
2022-12-26 21:03:28 -0600

    update spiketrain with merge

--- miv/typing.py
+++ miv/typing.py
@@ -5,10 +5,13 @@ import neo
 import numpy as np
 import numpy.typing as npt
 
+import miv
+import miv.core
+
 SignalType = Union[
     np.ndarray,
     neo.core.AnalogSignal,  # npt.DTypeLike
 ]  # Shape should be [signal_length, n_channel]
 TimestampsType = np.ndarray
-SpikestampsType = Union[np.ndarray, neo.core.SpikeTrain]
+SpikestampsType = Union[np.ndarray, neo.core.SpikeTrain, miv.core.Spikestamps]
 SpiketrainType = np.ndarray  # non-sparse boolean
