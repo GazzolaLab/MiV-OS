a799574de94a065e8595ec70ae51c25040905789
Seung Hyun Kim <skim449@illinois.edu>
2023-08-11 16:11:36 -0500

    resolve #311: add clean_cache CLI

---
+++ miv/machinary/clean_cache.py
@@ -0,0 +1,45 @@
+import glob
+import logging
+import pathlib
+import shutil
+
+import click
+
+
+@click.command()
+@click.option(
+    "--path",
+    "-p",
+    default=".",
+    type=click.Path(exists=True),
+    help="Path to clean cache files in.",
+)
+@click.option(
+    "--dry",
+    "-d",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help="Dry run. No files will be deleted.",
+)
+@click.option(
+    "--verbose",
+    "-v",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help="Verbose output.",
+)
+def clean_cache(path, dry, verbose):
+    """
+    Clean all cache files, recursively, in the given path.
+    """
+    if dry:
+        print("Dry run. No folders will be actually deleted.")
+
+    path = pathlib.Path(path) / "**/.cache"
+    for path in glob.glob("**/.cache", recursive=True):
+        if verbose:
+            print(f"Removing: {path}", flush=True)
+        if not dry:
+            shutil.rmtree(path)
