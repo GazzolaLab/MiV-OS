5079fbce4fdc38044765628cdf3d8ffa0dbb20b7
Seung Hyun Kim <skim449@illinois.edu>
2023-09-17 22:44:46 -0500

    test: add callback-defined plotting for generator-operator

--- tests/core/operator_generator/test_generator_callback.py
+++ tests/core/operator_generator/test_generator_callback.py
@@ -34,10 +34,28 @@ class MockGeneratorOperatorModule(GeneratorOperatorMixin):
             np.save(savepath, output)
 
 
+def generator_plot_test_callback(
+    self, output, inputs, show=False, save_path=None, index=-1
+):
+    savepath = os.path.join(save_path, f"gen_callback_{index}.npy")
+    # Save temporary file
+    if save_path is not None:
+        np.save(savepath, output)
+
+
+def firstiter_plot_test_callback(self, output, inputs, show=False, save_path=None):
+    savepath = os.path.join(save_path, "firstiter_callback_13.npy")
+    # Save temporary file
+    if save_path is not None:
+        np.save(savepath, output)
+
+
 def test_callback_firstiter_plot_from_callbacks(tmp_path):
     gen = GeneratorType([1, 2, 3, 4, 5])
     mock_operator = MockGeneratorOperatorModule()
     mock_operator.set_save_path(tmp_path / "results")
+    mock_operator << generator_plot_test_callback
+    mock_operator << firstiter_plot_test_callback
 
     gen >> mock_operator
     results = list(mock_operator.output())
@@ -46,6 +64,7 @@ def test_callback_firstiter_plot_from_callbacks(tmp_path):
 
     # Generator files
     for i, result in enumerate(results):
+        # In-class defined
         expected_file = os.path.join(
             tmp_path.as_posix(),
             "results",
@@ -53,12 +72,29 @@ def test_callback_firstiter_plot_from_callbacks(tmp_path):
             f"gen_test_{i}.npy",
         )
         assert os.path.exists(expected_file)
+        # Callback defined
+        expected_file = os.path.join(
+            tmp_path.as_posix(),
+            "results",
+            mock_operator.analysis_path,
+            f"gen_callback_{i}.npy",
+        )
+        assert os.path.exists(expected_file)
 
     # First-iteration files
+    # In-class defined
     expected_file = os.path.join(
         tmp_path.as_posix(),
         "results",
         mock_operator.analysis_path,
         "firstiter_test_9.npy",
+    )
+    assert os.path.exists(expected_file)
+    # Callback defined
+    expected_file = os.path.join(
+        tmp_path.as_posix(),
+        "results",
+        mock_operator.analysis_path,
+        "firstiter_callback_13.npy",
     )
     assert os.path.exists(expected_file)
