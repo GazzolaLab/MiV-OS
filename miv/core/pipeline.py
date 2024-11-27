4d5b2a3624e44aa4f7328714c7accc9feec838f1
Seung Hyun Kim <skim0119@gmail.com>
2024-11-27 01:52:15 -0600

    enable tmp directory usage for pipeline

--- miv/core/pipeline.py
+++ miv/core/pipeline.py
@@ -8,6 +8,7 @@ __all__ = ["Pipeline"]
 
 from typing import List, Optional, Union
 
+import os
 import pathlib
 import time
 
@@ -40,6 +41,7 @@ class Pipeline:
         self,
         working_directory: Union[str, pathlib.Path] = "./results",
         cache_directory: Optional[Union[str, pathlib.Path]] = None,
+        temporary_directory: Optional[Union[str, pathlib.Path]] = None,
         skip_plot: bool = False,
         verbose: bool = False,  # Use logging
     ):
@@ -53,6 +55,9 @@ class Pipeline:
         cache_directory : Optional[Union[str, pathlib.Path]], optional
             The cache directory where the pipeline will be executed. By default None
             If None, the cache directory will be the same as the working directory.
+        temporary_directory : Optional[Union[str, pathlib.Path]], optional
+            If given, files will be saved in temporary directory, and will be moved to
+            working directory after. Cache directory is not altered.
         verbose : bool, optional
             If True, the pipeline will log debugging informations. By default False
         """
@@ -61,7 +66,10 @@ class Pipeline:
             cache_directory = working_directory
         for node in self._start_node.topological_sort():
             if hasattr(node, "set_save_path"):
-                node.set_save_path(working_directory, cache_directory)
+                if temporary_directory is not None:
+                    node.set_save_path(temporary_directory, cache_directory)
+                else:
+                    node.set_save_path(working_directory, cache_directory)
 
         self.execution_order = [
             self._start_node
@@ -86,6 +94,11 @@ class Pipeline:
         if verbose:
             print(f"Pipeline done: computing {self._start_node}")
             print(self.summarize(), flush=True)
+
+        if temporary_directory is not None:
+            os.system(f"cp -rf {temporary_directory}/* {working_directory}/")
+            #import shutil
+            #shutil.move(temporary_directory, working_directory)
 
     def summarize(self):
         if self.execution_order is None:
