51af3bac0dab7b736cc9380312404bf5817a8f45
Seung Hyun Kim <skim0119@gmail.com>
2025-06-02 11:32:19 -0500

    feat: make vanilla generator call to be non-multiprocessing

--- miv/core/operator_generator/policy.py
+++ miv/core/operator_generator/policy.py
@@ -29,11 +29,54 @@ class VanillaGeneratorRunner:
     ) -> Generator:
         is_all_generator = all(inspect.isgenerator(v) for v in inputs)
         if not is_all_generator:
-            result = func(self, *inputs)
+            result = func(self, *inputs)  # FIXME: this will probably cause issue
             return result
 
         def generator_func(*args: tuple[Generator, ...]) -> Generator:
-            num_workers = 4
+            num_workers = 1
+            tasks = zip(*args, strict=False)
+            for idx, zip_arg in enumerate(tasks):
+                stime = time.time()
+                result = func(idx, *zip_arg)
+                print(f"    iter {idx:03d} {self}: {time.time() - stime:.03f}sec", flush=True)
+                yield result
+            print("generator-tasks done", flush=True)
+
+            # TODO: add lastiter_plot
+            # FIXME
+            self.parent._done_flag_generator_plot = True
+            self.parent._done_flag_firstiter_plot = True
+
+        generator = generator_func(*inputs)
+        return generator
+
+        # return func(*inputs)  # type: ignore
+
+    def get_run_order(self) -> int:
+        return 0
+
+class GeneratorRunnerInMultiprocessing:
+    """Default runner without any modification.
+    Simply, the operator will be executed in embarrassingly parallel manner.
+    If MPI is available, the operator will be executed in every ranks.
+
+    This runner is meant to be used for generator operators.
+    """
+
+    def __init__(self, parent, chunk_size:int=4):
+        self.parent = parent
+        self.chunk_size = chunk_size
+
+    def __call__(
+        self, func: _LazyCallable, inputs: list[Generator[Any]] | None = None
+    ) -> Generator:
+        is_all_generator = all(inspect.isgenerator(v) for v in inputs)
+        if not is_all_generator:
+            result = func(self, *inputs)  # FIXME: this will probably cause issue
+            return result
+
+        def generator_func(*args: tuple[Generator, ...]) -> Generator:
+            num_workers = self.chunk_size
             istart = 0
             tasks = zip(*args, strict=False)
             with mp.Pool(processes=num_workers) as pool:
