de4e05fda8b0981e6481e2084be9e15f612a8007
Seung Hyun Kim <skim0119@gmail.com>
2026-01-08 09:00:24 -0600

    multithread rate computation

--- miv/statistics/spiketrain_statistics.py
+++ miv/statistics/spiketrain_statistics.py
@@ -5,7 +5,7 @@ from typing import Any
 
 import matplotlib.pyplot as plt
 import numpy as np
-from numba import njit
+from numba import njit, prange
 import quantities as pq
 
 from miv.core import Spikestamps
@@ -250,10 +250,17 @@ def spike_counts_with_kernel(spiketrain, probe_times, kernel: Callable, batchsiz
 
     return result
 
+@njit(cache=True)
+def _kernel(x, amplitude=2.0, decay_rate=5):
+    # Exponential
+    # return amplitude * np.exp(-decay_rate * x) * decay_rate
+    # Alpha
+    return amplitude * np.exp(-decay_rate * x) * (decay_rate ** 2) * x
 
-@njit(parallel=True)
-def nb_spike_counts_with_kernel(
-    spiketrain, probe_times, kernel: Callable, batchsize=32
+
+@njit(cache=True, parallel=True)
+def decay_spike_counts(
+    spiketrain, probe_times,
 ):
     """
     Both spiketrain and probe_times should be a 1-d array representing time.
@@ -264,8 +271,6 @@ def nb_spike_counts_with_kernel(
         Single-channel spiketrain
     probe_times :
         probe_times
-    kernel :
-        kernel function
     """
     n_spike = spiketrain.size
     n_probe = probe_times.size
@@ -276,13 +281,13 @@ def nb_spike_counts_with_kernel(
 
     out = np.zeros(probe_times.shape, dtype=np.float64)
 
-    for i in range(n_spike):
+    for i in prange(n_spike):
         s = spiketrain[i]
         # first probe index with t >= s (requires sorted probe_times)
         j0 = np.searchsorted(probe_times, s)
         for j in range(j0, n_probe):
             dt = probe_times[j] - s  # dt >= 0 by construction
-            out[j] += kernel(dt)
+            out[j] += _kernel(dt)
 
     return out
 
@@ -299,20 +304,6 @@ def nb_spike_counts_with_kernel(
     # out += decay_count.sum(axis=0)
 
     # return out
-
-
-def decay_spike_counts(
-    spiketrain, probe_times, amplitude=1.0, decay_rate=5, batchsize=256
-):
-    @njit
-    def _decay_kernel(x):
-        return amplitude * np.exp(-decay_rate * x) * decay_rate
-
-    return nb_spike_counts_with_kernel(
-        np.asarray(spiketrain),
-        probe_times,
-        _decay_kernel,  # , batchsize
-    )
 
 
 def instantaneous_spike_rate(spiketrain, probe_times, window=1, batchsize=32):
